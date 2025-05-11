ac1983482e9600e05444883959db75a2d4a39218
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2025-05-11 20:56:18 +0900

    feat: 메인 페이지 링크 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <main className='flex flex-1 flex-col justify-center gap-8 p-8'>
+      <Link href='/fibo' className='text-4xl font-semibold'>
+        문제 1
+      </Link>
+      <Link href='/video' className='text-4xl font-semibold'>
+        문제 2
+      </Link>
+      <strong className='text-4xl font-semibold'>프론트엔드 강미정</strong>
+    </main>
+  );
 }
