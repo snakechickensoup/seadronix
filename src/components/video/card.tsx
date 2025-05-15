ad2a3e687c93bd5a86f3f3892421c51b1cb6653d
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2025-05-15 10:52:31 +0900

    refactor: 영상 재생 수정

--- src/components/video/card.tsx
+++ src/components/video/card.tsx
@@ -1,18 +1,7 @@
 'use client';
 
-import dynamic from 'next/dynamic';
 import { Card, CardHeader, CardTitle, CardContent } from '../ui/card';
-import { Skeleton } from '../ui/skeleton';
-
-const VideoPlayer = dynamic(() => import('./player'), {
-  ssr: false,
-  loading: () => (
-    <div className='flex w-full flex-1 flex-col items-end justify-end gap-4'>
-      <Skeleton className='h-12 w-24' />
-      <Skeleton className='h-full w-full' />
-    </div>
-  )
-});
+import VideoPlayer2 from './player2';
 
 const VideoCard = () => {
   return (
@@ -21,7 +10,7 @@ const VideoCard = () => {
         <CardTitle className='text-sm'>출력 결과</CardTitle>
       </CardHeader>
       <CardContent className='flex flex-1 flex-col items-end gap-4 overflow-hidden'>
-        <VideoPlayer />
+        <VideoPlayer2 />
       </CardContent>
     </Card>
   );
